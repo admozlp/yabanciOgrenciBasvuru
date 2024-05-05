09fc5a617feed73b40ecd4fbefca379357b932e9
adem <ozalp.adem@ogr.ahievran.edu.tr>
2024-05-05 14:18:48 +0300

    Java ve Spring versiyonları yükseltildi. Spring SecurityConfiguration.java da değişiklikler oldu.

--- src/main/java/com/ahievran/yabanciOgrenciBasvuru/config/SecurityConfiguration.java
+++ src/main/java/com/ahievran/yabanciOgrenciBasvuru/config/SecurityConfiguration.java
@@ -1,65 +1,72 @@
 package com.ahievran.yabanciOgrenciBasvuru.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ahievran.yabanciOgrenciBasvuru.business.abstracts.KisiKayitService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import com.ahievran.yabanciOgrenciBasvuru.business.abstracts.KisiKayitService;
+import static org.springframework.security.config.http.SessionCreationPolicy.ALWAYS;
 
-
-@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
-	@Autowired
-	private KisiKayitService kisiKayitService;
-	
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+	private static final String[] WHITE_LIST_URL = {
+			"/registration/**",
+			"/js/**",
+			"/css/**",
+			"/images/**",
+			"/kaydol/**",
+			"/img/**",
+			"/verifyaccount/**",
+			"/showerify/**",
+			"/forgetpassword/**",
+			"/sendmail/**",
+			"/basvurugoster/**",
+	};
+	private final KisiKayitService kisiKayitService;
+
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
-	public DaoAuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-		auth.setUserDetailsService(kisiKayitService);
-		auth.setPasswordEncoder(passwordEncoder());
-		return auth;
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(kisiKayitService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		return authenticationProvider;
 	}
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(authenticationProvider());
+
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		return http
+				.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(x ->
+						x.requestMatchers(WHITE_LIST_URL).permitAll()
+								.anyRequest().authenticated()
+				)
+				.sessionManagement(session -> session.sessionCreationPolicy(ALWAYS))
+				.authenticationProvider(authenticationProvider())
+				.formLogin(formLogin ->
+						formLogin.loginPage("/login").permitAll()
+								.defaultSuccessUrl("/", true)
+				)
+				.logout(logout -> logout.invalidateHttpSession(true).clearAuthentication(true)
+						.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout").permitAll())
+				.build();
+
 	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
-		
-		http.authorizeRequests()
-		.antMatchers("/registration**", "/js/**", "/css/**", "/images/**", "/kaydol/**", "/img/**",
-				"/verifyaccount/**", 	"/showerify/**", "/forgetpassword/**", "/sendmail/**")
-		.permitAll()
-		.anyRequest()
-				.authenticated()
-				.and().formLogin().
-				loginPage("/login").permitAll()				
-				.defaultSuccessUrl("/", true)				
-				.and().logout()
-				.invalidateHttpSession(true).
-				clearAuthentication(true)
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-				.logoutSuccessUrl("/login?logout")
-				.permitAll();
-	}
-	
 }
